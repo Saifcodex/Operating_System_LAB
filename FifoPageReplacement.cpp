f1d88909d56305c5a768670415f03bf8388ed258
Saif Ahmed <99961378+Saifcodex@users.noreply.github.com>
2025-04-22 13:20:58 +0600

    Add files via upload

---
+++ FifoPageReplacement.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+bool isInMemory(queue<int> memory, int page)
+{
+    while (!memory.empty()) 
+    {
+        if (memory.front() == page)
+        {
+            return true;
+        }
+        memory.pop();
+    }
+    return false;
+}
+int countPageMisses(int pages[], int totalPages, int frameSize) 
+{
+    queue<int> memory;
+    int misses = 0;
+    for (int i = 0; i < totalPages; i++) 
+    {
+        int currentPage = pages[i];
+        if (!isInMemory(memory, currentPage)) 
+        {
+            misses++;
+            if (memory.size() == frameSize) 
+            {
+                memory.pop();
+            }
+            memory.push(currentPage);
+        }
+    }
+    return misses;
+}
+int main() 
+{
+    int pages[] = {1, 3, 0, 3, 5, 6, 3};
+    int totalPages = sizeof(pages) / sizeof(pages[0]);
+    int frameSize = 4;
+    cout << countPageMisses(pages, totalPages, frameSize) << endl;
+    return 0;
+}
